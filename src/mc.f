d45fa7216f33b79e6fc23b568bfaa9ae7ee3a94d
David Cornelis Groothuizen Dijkema <d.groothuizen.dijkema@gmail.com>
2020-01-25 08:02:45 +1300

    Defined the control function which returns a vector of successive approximation of e

--- src/mc.f
+++ src/mc.f
@@ -13,12 +13,14 @@ module monte_carlo
 contains
 
   function simulate_e() result(cnt)
+    ! Defines the function of which e is the expected value
 
     integer :: cnt
     real(kind=8) :: sum,rand_num
 
     cnt=0
     sum=0
+    ! count the number of random numbers needed to sum to more than 1
     do while (sum.LE.1)
       call RANDOM_NUMBER(rand_num)
       sum=sum+rand_num
@@ -30,9 +32,24 @@ contains
   subroutine approximate_e(n_itr,means)
     !DEC$ ATTRIBUTES DLLEXPORT :: approximate_e
     !DEC$ ATTRIBUTES C :: approximate_e
-    implicit none
+    
     integer, intent(in) :: n_itr
     real(kind=8), intent(inout), dimension(n_itr) :: means
+
+    real(kind=8) :: mean
+    integer :: itr,approx
+
+    ! produce a sample of the simulate_e() function and keep a running mean
+    do itr=1,n_itr
+      approx=simulate_e()
+      if (itr.EQ.1) then
+        mean=approx
+      else
+        ! update the mean
+        mean=mean+(approx-mean)/(itr+1)
+      end if
+      means(itr)=mean
+    end do
 
   end subroutine approximate_e
 
