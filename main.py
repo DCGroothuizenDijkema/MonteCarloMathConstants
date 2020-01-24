dd4cf7b020a2e166b1f7d8fb91243401024a9458
David Cornelis Groothuizen Dijkema <d.groothuizen.dijkema@gmail.com>
2020-01-25 12:22:30 +1300

    Added simulation loop to run()

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ except OSError as err:
   if err.errno != errno.EEXIST:
     raise
 
-def run(constant,display_name):
+def run(constant,display_name,actual_value):
   if constant=='e':
     func=approximate_e
   elif constant=='pi':
@@ -53,6 +53,24 @@ def run(constant,display_name):
   _,ax=setup_simulation_plot(x_lim=(1,num_itr),title=r'Approximating {} with Monte Carlo simulations'.format(display_name)
     ,x_label='Iteration',y_label=r'Estimate of {}'.format(display_name))
 
+  # perform the simulation
+  mean=[]
+  last_run=int(num_runs/run_size)-1
+  data=np.empty((run_size,num_itr))
+  for run in range(int(num_runs/run_size)):
+    for sim in range(run_size):
+      if verbose:
+        print('Run: {}; Sim: {}'.format(run,sim))
+      data[sim,:]=func(num_itr)
+    # keep track of the means
+    mean.append(np.mean(data[:,-1]))
+    # if it's the last run, plot the actual value along with the data
+    # otherwise, just plot the data
+    if run==last_run:
+      ax=simulation_plot(data,np.arange(num_itr),ax=ax,hline=actual_value,hline_linewidth=1.5,hline_alpha=0.9)
+    else:
+      ax=simulation_plot(data,np.arange(num_itr),ax=ax)
+
 if __name__=='__main__':
-  run('e','Euler\'s Number')
-  # run('pi',r'$\pi$')
+  run('e','Euler\'s Number',np.e)
+  # run('pi',r'$\pi$',np.pi)
