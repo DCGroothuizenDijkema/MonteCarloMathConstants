2b9baa17e900f8434dd020f3259b5692a07e7519
David Cornelis Groothuizen Dijkema <d.groothuizen.dijkema@gmail.com>
2020-01-24 18:21:28 +1300

    Defined simulate_e() to return one approximation of e; changed function declaration to no longer accept random numbers

--- src/mc.f
+++ src/mc.f
@@ -1,7 +1,7 @@
 
 !+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+//
 !                                                                                                                                       //
-! frog.cpp                                                                                                                              //
+! mc.f                                                                                                                                  //
 !                                                                                                                                       //
 ! D. C. Groothuizen Dijkema - January, 2020                                                                                             //
 !+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+//
@@ -12,10 +12,18 @@ module monte_carlo
   implicit none
 contains
 
-  function simulate_e(random_nums,ind) result(cnt)
-    real(kind=8), dimension(:), intent(in) :: random_nums
-    integer, intent(inout) :: ind
+  function simulate_e() result(cnt)
+
     integer :: cnt
+    real(kind=8) :: sum,rand_num
+
+    cnt=0
+    sum=0
+    do while (sum.LE.1)
+      call RANDOM_NUMBER(rand_num)
+      sum=sum+rand_num
+      cnt=cnt+1
+    end do
 
   end function simulate_e
 
@@ -25,7 +33,7 @@ contains
     implicit none
     integer, intent(in) :: n_itr
     real(kind=8), intent(inout), dimension(n_itr) :: means
-    
+
   end subroutine approximate_e
 
 end module monte_carlo
