5188899d91c2ed3a85d88a01f8cf82e647f6a1a2
David Cornelis Groothuizen Dijkema <d.groothuizen.dijkema@gmail.com>
2020-01-25 11:30:01 +1300

    Created Python wrapper functions to approximate e and pi

--- mc.py
+++ mc.py
@@ -13,6 +13,8 @@ import numpy as np
 
 from huygens.interf import c_pointer,c_vector
 
+__all__=['approximate_e','approximate_pi']
+
 # load the lib
 _libc=ct.cdll.LoadLibrary('./bin/mc.dll')
 
@@ -25,3 +27,15 @@ _approximate_e.argtypes=[ct.POINTER(ct.c_int),ct.POINTER(ct.c_double)]
 _approximate_pi.argtypes=[ct.POINTER(ct.c_int),ct.POINTER(ct.c_double)]
 _approximate_e.restype=None
 _approximate_pi.restype=None
+
+def approximate_e(num_itr):
+  means=c_vector(ct.c_double,num_itr)
+  _approximate_e(c_pointer(ct.c_int,num_itr),means)
+  
+  return np.ctypeslib.as_array(means)
+
+def approximate_pi(num_itr):
+  means=c_vector(ct.c_double,num_itr)
+  _approximate_pi(c_pointer(ct.c_int,num_itr),means)
+  
+  return np.ctypeslib.as_array(means)
