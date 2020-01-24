3c9b6c2ef78ae123b0e0c6b1bb2d5a9b5e870231
David Cornelis Groothuizen Dijkema <d.groothuizen.dijkema@gmail.com>
2020-01-25 12:08:07 +1300

    Defined run() with function initialisation

--- main.py
+++ main.py
@@ -25,6 +25,30 @@ except OSError as err:
   if err.errno != errno.EEXIST:
     raise
 
+def run(constant):
+  if constant=='e':
+    func=approximate_e
+  elif constant=='pi':
+    func=approximate_pi
+  else:
+    raise ValueError('Functions only exist to approximate e and pi.')
+
+  # constants
+  num_itr=1000000
+  num_runs=150
+  run_size=10
+
+  assert num_runs%run_size==0, "`num_runs` is not a multiple of `run_size`"
+
+  # output
+  plot_name='{}.png'.format(constant)
+  image_res=1200
+
+  # control
+  save_fig=True
+  show_fig=False
+  verbose=True
 
 if __name__=='__main__':
-  pass
+  run('e')
+  # run('pi')
