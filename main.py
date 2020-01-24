7137e9e6d2ee578236044860d2b180980c282fa2
David Cornelis Groothuizen Dijkema <d.groothuizen.dijkema@gmail.com>
2020-01-25 11:50:11 +1300

    Created template for main execution file

--- main.py
+++ main.py
@@ -7,3 +7,24 @@
 #+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+//
 
 # Main file to produce approximations of mathematical constants using Monte Carlo methods
+
+
+import errno
+import os
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+from huygens.plotting import simulation_plot,setup_simulation_plot
+from mc import *
+
+output_dir='./out/' # change if you want the plots somewhere else
+try:
+  os.makedirs(output_dir) 
+except OSError as err:
+  if err.errno != errno.EEXIST:
+    raise
+
+
+if __name__=='__main__':
+  pass
